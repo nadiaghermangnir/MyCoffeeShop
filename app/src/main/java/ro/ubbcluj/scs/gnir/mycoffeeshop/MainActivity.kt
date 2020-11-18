5bcd62118abe97736a5962b269d2e14a4c2d43f7
nadiagherman <nadia.gherman@yahoo.com>
2020-11-18 17:00:14 +0200

    final lab 2

--- app/src/main/java/ro/ubbcluj/scs/gnir/mycoffeeshop/MainActivity.kt
+++ app/src/main/java/ro/ubbcluj/scs/gnir/mycoffeeshop/MainActivity.kt
@@ -1,11 +1,31 @@
 package ro.ubbcluj.scs.gnir.mycoffeeshop
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import ro.ubbcluj.scs.gnir.mycoffeeshop.core.TAG
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        Log.i(TAG, "onCreate")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
